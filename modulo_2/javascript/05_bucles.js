012cb0a333d7ebad888d2441065a1bfd9efb65bf
maria <mariajosetacoc2005@gmail.com>
2025-10-21 10:03:45 -0500

    clase 2025-10-21

---
+++ modulo_2/javascript/05_bucles.js
@@ -0,0 +1,20 @@
+for(let i=1; i <=10; i++) {
+    console.log("Número:", i);
+}
+suma=0;
+for(let i=1; i <=10; i++) {
+    suma += i;
+}
+console.log("Sumatroria indice", suma);
+
+
+for (let i=10; i >=1; i--) {
+    console.log("multiplicacoones:", i);
+    console.log("Resultado:", i*4);
+}
+console.log("Tabla del 4");
+
+let nombre = "francisco";
+for (let i=0; i <=nombre.length; i++) {
+    console.log(nombre[i]);
+}
